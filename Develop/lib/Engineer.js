459949a5298e1651cc9e4c87e87231243050e713
Roseanne Phillips <rphillips.rp@gmail.com>
2021-02-24 09:27:36 -0500

    all tests pass yay

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,7 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
-//calling to the parent constructor employee to access properties and methods
+//calling to the parent employee class to access properties and methods
 class Engineer extends Employee {
     constructor(name, id, email, github){
         super(name, id, email);
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,12 +1,13 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
+//calling to the parent employee class to access properties and methods
 class Intern extends Employee {
     constructor (name, id, email, school) {
         super (name, id, email);
         this.school = school;
     }
-
+//adding intern role
     getRole() {
         return "Intern";
 
