75b4686bc72f83dfa50a0e9e4b7530fcc97115c9
Roseanne Phillips <rphillips.rp@gmail.com>
2021-02-24 08:28:11 -0500

    failed engineer test

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
 
+//calling to the parent constructor employee to access properties and methods
 class Engineer extends Employee {
     constructor(name, id, email, github){
-        
+        super(name, id, email);
+        this.github = github;
+    }
+    getRole() {
+        return "Engineer";
+
+    }
+    getGithub() {
+        return this.github;
+
     }
 }
